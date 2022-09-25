8b3c7e21c654671f8e6571f5d4ab8e5350cf8151
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-09-25 19:43:25 +0300

    Completed Assignment

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddTransactionForm() {
+  const [transaction, setTransaction] = useState([]); 
+  const [newData, setNewData] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: 0,
+  })
+
+  function handleSubmit(event){
+    event.preventDefault()
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newData)
+    })
+      .then((r) => r.json())
+      .then((dataObj) => {
+      const newTransactionObj = [...transaction, dataObj] 
+      setTransaction(newTransactionObj)
+      })
+  }
+
+  function handleChange(e){
+    let name = e.target.name
+    let value = e.target.value
+    setNewData ({...newData, [name]: value });
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit={handleSubmit} className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" onChange = {handleChange}  />
+          <input type="text" name="description" placeholder="Description" onChange = {handleChange} />
+          <input type="text" name="category" placeholder="Category" onChange = {handleChange} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" onChange = {handleChange} />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Search() {
+function Search({setSearch}) {
+  
+  function handleSearch (e){
+    setSearch(e.target.value);
+  }
+
   return (
     <div className="ui large fluid icon input">
-      <input
-        type="text"
-        placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+      <input 
+      type="text" 
+      placeholder="Search your Recent Transactions" 
+      onChange={handleSearch} 
       />
       <i className="circular search link icon"></i>
     </div>
